8b26dabe80e3176ec6a9c76a2f5118746941719e
Pooria DANAEIFAR <pouryadanaeifar@gmail.com>
2023-03-08 17:31:04 +0100

    Test for fvSolution

--- tests/test_tgv.py
+++ tests/test_tgv.py
@@ -1,6 +1,13 @@
-from fluidsimfoam_tgv import Simul
+from pathlib import Path
 
+from fluidsimfoam_tgv import Simul
 from fluidsimfoam import load
+
+here = Path(__file__).absolute().parent
+
+path_tiny = here / "pure_openfoam_cases/tiny-tgv"
+
+path_fvsolution = path_tiny / "system/fvSolution"
 
 
 def test_init_simul():
@@ -20,3 +27,13 @@ def test_init_simul():
     sim2 = load(sim.path_run)
 
     assert sim2.path_run == sim.path_run
+
+    text_manual = path_fvsolution.read_text()
+
+    path_sim_fvsolution = sim.path_run / "system/fvSolution"
+
+    assert path_sim_fvsolution.exists()
+
+    text_produced = path_sim_fvsolution.read_text()
+
+    assert text_manual == text_produced
