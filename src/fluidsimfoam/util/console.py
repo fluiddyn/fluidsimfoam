f5ba0cc0ee65abaae0c4978e764455f3bf536d31
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-03-08 23:32:10 +0100

    fluidsimfoam/util/console.py

---
+++ src/fluidsimfoam/util/console.py
@@ -0,0 +1,45 @@
+"""Console script functions
+
+"""
+
+from textwrap import dedent
+
+from fluidsimfoam.solvers import available_solvers
+
+
+def print_versions():
+    import fluiddyn
+    import fluidsim_core
+    import fluidsimfoam
+
+    versions = {"Package": "Version", "-------": "-------"}
+
+    packages = [fluidsimfoam, fluiddyn, fluidsim_core]
+    for package in packages:
+        versions[package.__name__] = package.__version__
+
+    for pkg_name, version in versions.items():
+        print(f"{pkg_name.ljust(15)} {version}")
+
+    names = sorted(set([entry_point.name for entry_point in available_solvers()]))
+    print("\nInstalled solvers: " + ", ".join(names))
+
+
+def start_ipython_load_sim():
+    """Start IPython and load a simulation"""
+    from IPython import start_ipython
+
+    argv = ["--matplotlib", "-i", "-c"]
+    code = dedent(
+        """
+        import numpy as np
+        import matplotlib.pyplot as plt
+        from fluidsimfoam import load
+        print("Loading simulation")
+        sim = load()
+        params = sim.params
+        print("`sim`, `params`, `np`, `plt` and `pd` variables are available")
+    """
+    )
+    argv.append("; ".join(code.strip().split("\n")))
+    start_ipython(argv=argv)
