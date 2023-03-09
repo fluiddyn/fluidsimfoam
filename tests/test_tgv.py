81b1cadb264a3368335d929a9383293a998aaa0a
Pooria DANAEIFAR <pouryadanaeifar@gmail.com>
2023-03-09 15:45:55 +0100

    Check the creation of fvSchemes

--- tests/test_tgv.py
+++ tests/test_tgv.py
@@ -7,8 +7,6 @@ here = Path(__file__).absolute().parent
 
 path_tiny = here / "pure_openfoam_cases/tiny-tgv"
 
-path_fvsolution = path_tiny / "system/fvSolution"
-
 
 def test_init_simul():
     params = Simul.create_default_params()
@@ -27,13 +25,13 @@ def test_init_simul():
     sim2 = load(sim.path_run)
 
     assert sim2.path_run == sim.path_run
+    for name in ("fvSolution", "fvSchemes"):
+        path_produced = sim.path_run / "system" / name
+        path_manual = path_tiny / "system" / name
 
-    text_manual = path_fvsolution.read_text()
+        text_manual = path_manual.read_text()
+        assert path_produced.exists()
 
-    path_sim_fvsolution = sim.path_run / "system/fvSolution"
+        text_produced = path_produced.read_text()
 
-    assert path_sim_fvsolution.exists()
-
-    text_produced = path_sim_fvsolution.read_text()
-
-    assert text_manual == text_produced
+        assert text_manual == text_produced
