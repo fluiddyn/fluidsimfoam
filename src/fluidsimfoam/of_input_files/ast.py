c9d8e88e830573e6e77a20cbe6953d134eff9f3b
Pooria DANAEIFAR <pouryadanaeifar@gmail.com>
2023-03-17 14:13:51 +0100

    Start putting parser in Fluidsimfoam

---
+++ src/fluidsimfoam/of_input_files/ast.py
@@ -0,0 +1,4 @@
+
+
+class OFInputFile():
+    pass
---
+++ src/fluidsimfoam/of_input_files/parser.py
@@ -0,0 +1,41 @@
+def parse():
+
+    from lark import Lark, Transformer, Token
+
+    grammar = r"""
+        ?value: CNAME
+            | dict_assignment
+            | nested_dict
+            | list
+            | file
+            | assignment
+            | string
+            | CPP_COMMENT
+            | C_COMMENT
+            | SIGNED_NUMBER      -> number
+        string : ESCAPED_STRING
+        keyword : CNAME
+        dataentry : CNAME | value | list | string
+        assignment : keyword dataentry ";" NEWLINE
+        dict_assignment : CNAME NEWLINE "{" NEWLINE [assignment (assignment)*] "}" NEWLINE
+        nested_dict : CNAME NEWLINE "{" NEWLINE (dict_assignment)* "}" NEWLINE
+        list: "(" (value|NEWLINE)* ")"
+        list_assignment: CNAME NEWLINE list ";" NEWLINE
+
+        file : [(dict_assignment | nested_dict | assignment | list_assignment)*]
+        CPP_COMMENT: /\/\/[^\n]*/ NEWLINE
+        C_COMMENT: "/*" /(.|\n)*?/ "*/" NEWLINE
+
+        %ignore /[\n\f\r]+/
+        %ignore CPP_COMMENT
+        %ignore C_COMMENT
+        %import common.SIGNED_NUMBER
+        %import common.ESCAPED_STRING
+        %import common.NUMBER
+        %import common.WS
+        %ignore WS
+        %import common.NEWLINE
+        %import common.CNAME
+        """
+
+    
