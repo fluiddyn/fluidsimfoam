4f77c896fa4762e8358a6d5b1fe14871c7d73780
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-03-22 23:16:55 +0100

    Clean up ast code with better testing

--- src/fluidsimfoam/of_input_files/ast.py
+++ src/fluidsimfoam/of_input_files/ast.py
@@ -1,4 +1,55 @@
+import re
 from dataclasses import dataclass
+
+symbols = ["kg", "m", "s", "K", "kmol", "A", "cd"]
+
+
+def str2of_units(units):
+    result = [0] * 7
+    sign = 1
+    while units:
+        try:
+            index = re.search(r"[/.]", units).start()
+        except AttributeError:
+            unit_all = units
+            units = ""
+            next_oper = "."
+        else:
+            next_oper = units[index]
+            assert next_oper in "/."
+            unit_all = units[:index]
+
+        units = units[index + 1 :]
+        if "^" in unit_all:
+            unit_name, unit_value = unit_all.split("^")
+            unit_value = int(unit_value)
+        else:
+            unit_name, unit_value = unit_all, 1
+        if unit_name != "1":
+            unit_index = symbols.index(unit_name)
+            result[unit_index] = sign * unit_value
+        sign = 1 if next_oper == "." else -1
+    return result
+
+
+def of_units2str(of_units):
+    if len(of_units) != len(symbols):
+        raise ValueError("len(of_units) != len(symbols)")
+    result = []
+    for symbol, exponent in zip(symbols, of_units):
+        if exponent == 0:
+            continue
+        operator = "." if exponent > 0 else "/"
+        if abs(exponent) == 1:
+            result.append(f"{operator}{symbol}")
+        else:
+            result.append(f"{operator}{symbol}^{abs(exponent)}")
+    result = "".join(result)
+    if result.startswith("."):
+        result = result[1:]
+    elif result.startswith("/"):
+        result = "1" + result
+    return result
 
 
 class Node:
@@ -39,82 +90,31 @@ class Value(Node):
         self.value = value
         self.name = name
 
-        s_dim = ""
-
-        if dimension is not None:
-            symbols = ["kg", "m", "s", "K", "kmol", "A", "cd"]
-
+        if isinstance(dimension, (list, tuple)):
             if len(dimension) != len(symbols):
                 raise ValueError("len(dimension) != len(symbols)")
-
-            numerator = []
-            denominator = []
-            for i, d in enumerate(dimension):
-                if d > 0:
-                    if d == 1:
-                        numerator.append(symbols[i])
-                    else:
-                        numerator.append(symbols[i] + "^" + str(d))
-                elif d < 0:
-                    if d == -1:
-                        denominator.append(symbols[i])
-                    else:
-                        denominator.append(symbols[i] + "^" + str(-d))
-
-            if numerator:
-                s_dim += "".join(numerator)
-                if denominator:
-                    s_dim += "/" + "".join(denominator)
-            else:
-                if denominator:
-                    s_dim += "1/" + "".join(denominator)
-
-        self.dimension = s_dim
+            dimension = of_units2str(dimension)
+        self.dimension = dimension
 
     def __repr__(self):
-        if self.dimension is not None:
+        if self.dimension is not None and self.name is not None:
             return f'Value({self.value}, name="{self.name}", dimension="{self.dimension}")'
-        else:
+        elif self.dimension is None and self.name is not None:
             return f'Value({self.value}, name="{self.name}")'
+        elif self.dimension is not None and self.name is None:
+            return f'Value({self.value}, dimension="{self.dimension}")'
+        else:
+            return f"Value({self.value})"
 
     def dump(self):
         if self.dimension is not None:
-            numerator = self.dimension.split("/")[0]
-            denominator = self.dimension.split("/")[1]
-            symbols = ["kg", "m", "s", "K", "kmol", "A", "cd"]
-            dim = [0, 0, 0, 0, 0, 0, 0]
-
-            for i, symb in enumerate(symbols):
-                s = symb + "^"
-                if s in numerator:
-                    a = numerator.find(s)
-                    dim[i] = int(numerator[a + len(symb) + 1])
-                    numerator = numerator.replace(
-                        s + numerator[a + len(symb) + 1], ""
-                    )
-
-                if s in denominator:
-                    a = denominator.find(s)
-                    dim[i] = -int(denominator[a + len(symb) + 1])
-                    denominator = denominator.replace(
-                        s + denominator[a + len(symb) + 1], ""
-                    )
-
-            for i, symb in reversed(list(enumerate(symbols))):
-                if symb in numerator:
-                    a = numerator.find(symb)
-                    dim[i] = 1
-                    numerator = numerator.replace(symb, "")
-
-                if symb in denominator:
-                    a = denominator.find(symb)
-                    dim[i] = -1
-                    denominator = denominator.replace(symb, "")
-
-            dim = str(dim).replace(",", "")
-            dim = dim.replace("[", "[ ")
-            dim = dim.replace("]", " ]")
-
-            return f"{self.name} {dim} {self.value};"
-        else:
+            dimension_list = str2of_units(self.dimension)
+            dimension_dumped = " ".join(str(number) for number in dimension_list)
+        if self.dimension is not None and self.name is not None:
+            return f"{self.name} [{dimension_dumped}] {self.value};"
+        elif self.dimension is None and self.name is not None:
             return f"{self.name} {self.value};"
+        elif self.dimension is not None and self.name is None:
+            return f"[{dimension_dumped}] {self.value};"
+        else:
+            return f"{self.value};"
--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,10 +1,12 @@
-from fluidsimfoam.of_input_files.ast import Value
+import pytest
+
+from fluidsimfoam.of_input_files.ast import Value, of_units2str, str2of_units
 
 
 def test_value():
     value = Value(1e-06, name="nu", dimension=[0, 2, -1, 0, 0, 0, 0])
     assert repr(value) == 'Value(1e-06, name="nu", dimension="m^2/s")'
-    assert value.dump() == "nu [ 0 2 -1 0 0 0 0 ] 1e-06;"
+    assert value.dump() == "nu [0 2 -1 0 0 0 0] 1e-06;"
 
     value = Value(1e-06, name="nu")
     assert repr(value) == 'Value(1e-06, name="nu")'
@@ -12,4 +14,22 @@ def test_value():
 
     value = Value(1e-06, dimension=[0, 2, -1, 0, 0, 0, 0])
     assert repr(value) == 'Value(1e-06, dimension="m^2/s")'
-    assert value.dump() == "[ 0 2 -1 0 0 0 0 ] 1e-06;"
+    assert value.dump() == "[0 2 -1 0 0 0 0] 1e-06;"
+
+
+def test_of_units2str():
+    assert of_units2str([0, 2, -1, 0, 0, 0, 0]) == "m^2/s"
+    assert of_units2str([-1, 2, -1, 0, 0, 0, 0]) == "1/kg.m^2/s"
+
+
+def test_str2of_units():
+    assert str2of_units("m^2/s") == [0, 2, -1, 0, 0, 0, 0]
+    assert str2of_units("1/kg.m^2/s") == [-1, 2, -1, 0, 0, 0, 0]
+
+
+@pytest.mark.parametrize(
+    "of_units",
+    ([0, 0, 0, 0, 0, 0, 0], [2, 2, -1, 0, 0, 0, 0], [-1, 2, -1, 3, 1, -2, -1]),
+)
+def test_convert_unit_format(of_units):
+    assert str2of_units(of_units2str(of_units)) == of_units
