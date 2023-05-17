1776aea9847a38de1419cdf91c0b2c332bfa0799
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-05-17 14:56:57 +0200

    Improve fvOptions API

--- src/fluidsimfoam/foam_input_files/fv_options.py
+++ src/fluidsimfoam/foam_input_files/fv_options.py
@@ -30,34 +30,140 @@ def _make_default_params_dict(default, name_parameters, result):
     return result
 
 
+def get_selection_mode(cell_zone, cell_set, points):
+    is_not_none = [var is not None for var in (cell_zone, cell_set, points)]
+    nb_var_not_none = sum(is_not_none)
+
+    if nb_var_not_none == 0:
+        return "all"
+    elif nb_var_not_none > 1:
+        raise ValueError
+
+    if cell_zone is not None:
+        selection_mode = "cellZone"
+    elif cell_set is not None:
+        selection_mode = "cellSet"
+    elif points is not None:
+        selection_mode = "points"
+    else:
+        raise RuntimeError
+
+    return selection_mode
+
+
 class FvOption:
-    def __init__(self, name, default, parameters=None):
+    def __init__(
+        self,
+        type,
+        name,
+        active=True,
+        cell_zone=None,
+        cell_set=None,
+        points=None,
+        coeffs=None,
+        parameters=None,
+        default=None,
+    ):
+        self.type = type
         self.name = name
+
+        if parameters is None:
+            parameters = set()
+        else:
+            parameters = set(parameters)
+
+        if default is None:
+            default = {}
+
+        default["type"] = type
+        self.active = active
+
+        self.coeffs = coeffs
+        if coeffs is not None:
+            if "coeffs" in default:
+                raise ValueError
+            default["coeffs"] = coeffs
+            self.name_coeffs_key = type + "Coeffs"
+
+        self.cell_zone = cell_zone
+        self.cell_set = cell_set
+        self.points = points
+
         self.default = default
         self.parameters = parameters
 
     def complete_params(self, params_fv_options):
-        default_params = {}
+        default_params = {
+            "active": self.active,
+            "selection": {
+                "cell_zone": self.cell_zone,
+                "cell_set": self.cell_set,
+                "points": self.points,
+            },
+        }
         _make_default_params_dict(self.default, self.parameters, default_params)
+        if "__coeffs__" in default_params:
+            default_params["coeffs"] = default_params.pop("__coeffs__")
         _complete_params_dict(params_fv_options, self.name, default_params)
 
-    def complete_tree(self, tree, params_fv_options):
+    def get_dict_for_tree(self, params_fv_options):
         params_option = params_fv_options[_as_py_name(self.name)]
 
-        default = deepcopy(self.default)
-        _update_dict_with_params(default, params_option)
+        dict_for_tree = {"type": self.type, "active": None}
 
-        tree.init_from_py_objects({self.name: default})
+        select = params_option.selection
+        dict_for_tree["selectionMode"] = get_selection_mode(
+            select.cell_zone, select.cell_set, select.points
+        )
 
-        return tree
+        for key in ("cellZone", "cellSet", "points"):
+            key_as_py_name = _as_py_name(key)
+            if select[key_as_py_name] is not None:
+                dict_for_tree[key] = select[key_as_py_name]
+
+        dict_for_tree.update(self.default)
+
+        _update_dict_with_params(dict_for_tree, params_option)
+
+        active = params_option.active
+        if isinstance(active, bool):
+            active = ("no", "yes")[int(active)]
+        dict_for_tree["active"] = active
+        if self.coeffs is not None:
+            dict_for_tree[self.name_coeffs_key] = dict_for_tree.pop("coeffs")
+        return dict_for_tree
 
 
 class FvOptionsHelper(FileHelper):
     def __init__(self):
         self.options = {}
 
-    def add_option(self, name, default, parameters=None):
-        self.options[name] = FvOption(name, default, parameters)
+    def add_option(
+        self,
+        type,
+        name=None,
+        active=True,
+        cell_zone=None,
+        cell_set=None,
+        points=None,
+        coeffs=None,
+        parameters=None,
+        default=None,
+    ):
+        if name is None:
+            name = type
+
+        self.options[name] = FvOption(
+            type,
+            name,
+            active,
+            cell_zone,
+            cell_set,
+            points,
+            coeffs,
+            parameters,
+            default,
+        )
 
     def remove_option(self, name):
         del self.options[name]
@@ -73,10 +179,12 @@ class FvOptionsHelper(FileHelper):
                 "version": 2.0,
                 "format": "ascii",
                 "class": "dictionary",
-                "location": '"constant"',
+                "location": '"system"',
                 "object": "fvOptions",
             }
         )
-        for option in self.options.values():
-            option.complete_tree(tree, params.fv_options)
+        for name, option in self.options.items():
+            tree.init_from_py_objects(
+                {name: option.get_dict_for_tree(params.fv_options)}
+            )
         return tree
