766d9fc3dbd7d82f1ea48315501f45706d72cb46
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-05-22 14:21:52 +0200

    fluidsimfoam.testing.skipif_executable_not_available

--- doc/examples/fluidsimfoam-phill/tests/test_phill.py
+++ doc/examples/fluidsimfoam-phill/tests/test_phill.py
@@ -1,16 +1,11 @@
-import shutil
 from pathlib import Path
 
-import pytest
 from fluidsimfoam_phill import Simul
 
-from fluidsimfoam.testing import check_saved_case
-
-here = Path(__file__).absolute().parent
-
-path_saved_case = here / "saved_cases/case0"
+from fluidsimfoam.testing import check_saved_case, skipif_executable_not_available
 
 
+@skipif_executable_not_available("postProcess")
 def test_reproduce_case():
     params = Simul.create_default_params()
     params.output.sub_directory = "tests_fluidsimfoam/phill"
@@ -18,15 +13,11 @@ def test_reproduce_case():
     params.block_mesh_dict.ny = 7
     params.block_mesh_dict.ny_porosity = 4
     sim = Simul(params)
-    check_saved_case(path_saved_case, sim.path_run)
+    here = Path(__file__).absolute().parent
+    check_saved_case(here / "saved_cases/case0", sim.path_run)
 
 
-path_foam_executable = shutil.which("icoFoam")
-
-
-@pytest.mark.skipif(
-    path_foam_executable is None, reason="executable icoFoam not available"
-)
+@skipif_executable_not_available("icoFoam")
 def test_run():
     params = Simul.create_default_params()
     params.output.sub_directory = "tests_fluidsimfoam/phill"
