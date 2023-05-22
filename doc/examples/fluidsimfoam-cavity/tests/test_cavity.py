f8aed182b22fddb987f0825776c9410c3ac78bb4
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-05-22 16:37:02 +0200

    Modernize test_cavity.py

--- doc/examples/fluidsimfoam-cavity/tests/test_cavity.py
+++ doc/examples/fluidsimfoam-cavity/tests/test_cavity.py
@@ -1,29 +1,20 @@
-import shutil
 from pathlib import Path
 
-import pytest
 from fluidsimfoam_cavity import Simul
 
-from fluidsimfoam.testing import check_saved_case
+from fluidsimfoam.testing import check_saved_case, skipif_executable_not_available
 
 here = Path(__file__).absolute().parent
-
-path_saved_case = here / "saved_cases/cavity"
 
 
 def test_reproduce_case():
     params = Simul.create_default_params()
     params.output.sub_directory = "tests_fluidsimfoam/cavity"
     sim = Simul(params)
-    check_saved_case(path_saved_case, sim.path_run)
+    check_saved_case(here / "saved_cases/cavity", sim.path_run)
 
 
-path_foam_executable = shutil.which("icoFoam")
-
-
-@pytest.mark.skipif(
-    path_foam_executable is None, reason="executable icoFoam not available"
-)
+@skipif_executable_not_available("icoFoam")
 def test_run():
     params = Simul.create_default_params()
     params.output.sub_directory = "tests_fluidsimfoam/cavity"
