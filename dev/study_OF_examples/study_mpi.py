95a1c142e6f21de075f5963d700eb93b7ebeef71
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-05-23 08:47:22 +0200

    dev/study_OF_examples/study_mpi.py

---
+++ dev/study_OF_examples/study_mpi.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+from pprint import pprint
+
+from fluidsimfoam.foam_input_files import dump, parse
+
+tutorials_dir = Path(os.environ["FOAM_TUTORIALS"])
+
+tutorials_dir = Path.home() / "Dev/sedfoam/tutorials/"
+
+print(f"{tutorials_dir = }")
+
+methods = set()
+
+
+for path in tutorials_dir.rglob("*"):
+    if path.is_dir() or path.name != "decomposeParDict":
+        continue
+
+    print(path)
+
+    text = path.read_text()
+
+    tree = parse(text)
+
+    try:
+        method = tree.children["method"]
+    except (TypeError, KeyError):
+        method = None
+        print("no method!!!")
+        continue
+
+    methods.add(method)
+
+    code = dump(tree)
+
+    # print(code)
+
+
+print("types:")
+pprint(methods)
