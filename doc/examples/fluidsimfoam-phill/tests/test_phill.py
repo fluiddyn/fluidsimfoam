3bf93f83fadd646b6652e33e80157208137ffeda
Pooria DANAEIFAR <pouryadanaeifar@gmail.com>
2023-05-23 16:26:46 +0200

    Add test for sinus and 2d_phill

--- doc/examples/fluidsimfoam-phill/tests/test_phill.py
+++ doc/examples/fluidsimfoam-phill/tests/test_phill.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
+import pytest
 from fluidsimfoam_phill import Simul
+from fluidsimfoam_phill.blockmesh import make_code_2d_phill, make_code_sinus
 
+from fluidsimfoam.foam_input_files import dump, parse
 from fluidsimfoam.testing import check_saved_case, skipif_executable_not_available
 
 
@@ -24,3 +27,32 @@ def test_run():
     params.control_dict.end_time = 0.001
     sim = Simul(params)
     sim.make.exec("run")
+
+
+path_data = Path(__file__).absolute().parent / "saved_cases"
+
+
+@pytest.mark.parametrize(
+    "name",
+    ["sinus", "2d_phill"],
+)
+def test_blockmesh(name):
+    params = Simul.create_default_params()
+
+    if name == "sinus":
+        params.block_mesh_dict.nx = 11
+        params.block_mesh_dict.ny = 7
+        params.block_mesh_dict.ny_porosity = 4
+        path_saved_file = path_data / "case0/system/blockMeshDict"
+    elif name == "2d_phill":
+        path_saved_file = path_data / "blockmeshdicts/blockMeshDict_2d_phill"
+
+    make_blockmesh = globals()["make_code_" + name]
+    code_from_py = make_blockmesh(params.block_mesh_dict)
+
+    code_saved = path_saved_file.read_text()
+
+    tree_saved_file = parse(code_saved)
+    tree_from_py = parse(code_from_py)
+
+    assert dump(tree_saved_file).strip() == dump(tree_from_py).strip()
