44e7c6e4f35b0d45586f6cc50b60d0bac6f35b12
Pooria DANAEIFAR <pouryadanaeifar@gmail.com>
2023-05-30 17:39:47 +0200

    Add script for 3d_phill

---
+++ doc/examples/fluidsimfoam-phill/doc/reproduce_3d_phill.py
@@ -0,0 +1,34 @@
+from fluidsimfoam_phill import Simul
+
+params = Simul.create_default_params()
+
+params.output.sub_directory = "examples_fluidsimfoam/phill"
+params.short_name_type_run = "sin_3d"
+
+params.init_fields.buoyancy_frequency = 0.001
+params.transport_properties.nu = 0.01
+params.transport_properties.pr = 10
+
+params.control_dict.end_time = 86000
+params.control_dict.delta_t = 10
+params.control_dict.write_interval = 5000
+
+params.block_mesh_dict.geometry = "3d_phill"
+params.block_mesh_dict.lx = 10
+params.block_mesh_dict.ly = 10
+params.block_mesh_dict.lz = 10
+params.block_mesh_dict.h_max = 3
+params.block_mesh_dict.ly_porosity = 10
+
+params.block_mesh_dict.nx = 30
+params.block_mesh_dict.ny = 30
+params.block_mesh_dict.nz = 30
+params.block_mesh_dict.nz_p = 10
+
+params.fv_options.momentum_source.active = False
+params.fv_options.atm_coriolis_u_source.active = False
+params.fv_options.porosity.active = False
+
+sim = Simul(params)
+
+sim.make.exec("run")
