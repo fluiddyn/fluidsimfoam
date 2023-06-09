d69710a4b47b509f92dc9c3b90b46274db5b7ac5
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-06-09 14:20:54 +0200

    Improve fluidsimfoam-dam/doc/tuto_simple.py

--- doc/examples/fluidsimfoam-dam/doc/tuto_simple.py
+++ doc/examples/fluidsimfoam-dam/doc/tuto_simple.py
@@ -2,15 +2,20 @@ from fluidsimfoam_dam import Simul
 
 params = Simul.create_default_params()
 
-params.output.sub_directory = "poster_fluidsimfoam/dam"
-params.control_dict.end_time = 1.0
+params.output.sub_directory = "tuto_fluidsimfoam/dam"
+params.control_dict.end_time = 4.0
 
 params.parallel.method = "simple"
-params.parallel.nsubdoms = 4
-params.parallel.nsubdoms_xyz = [2, 2, 1]
+params.parallel.nsubdoms = 2
+params.parallel.nsubdoms_xyz = [2, 1, 1]
+
+params.block_mesh_dict.height_dam = 0.5
+params.block_mesh_dict.width_dam = 0.2
 
 params.constant.transport.water.nu = 2.0e-6
-...
+
+params.block_mesh_dict.nx = 80
+params.block_mesh_dict.ny = 80
 
 # creation of the simulation directory
 sim = Simul(params)
