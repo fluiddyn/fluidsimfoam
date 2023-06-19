d6e5c8fd3de498dc7b52d15dd0371599504f9618
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-06-19 10:54:27 +0200

    fluidsimfoam_multi_region_snappy controlDict

--- doc/examples/fluidsimfoam-multi-region-snappy/src/fluidsimfoam_multi_region_snappy/output.py
+++ doc/examples/fluidsimfoam-multi-region-snappy/src/fluidsimfoam_multi_region_snappy/output.py
@@ -16,9 +16,28 @@ class OutputMultiRegionSnappy(Output):
     ]
     name_constant_files = ["g", "regionProperties"]
 
-    _helper_control_dict = None
-    # can be replaced by:
-    # _helper_control_dict = Output._helper_control_dict.new()
+    _helper_control_dict = Output._helper_control_dict.new(
+        """
+        application       chtMultiRegionFoam
+        startFrom         latestTime
+        startTime         0.001
+        stopAt            endTime
+        endTime           75
+        deltaT            0.001
+        writeControl      adjustable
+        writeInterval     15
+        purgeWrite        0
+        writeFormat       ascii
+        writePrecision    7
+        writeCompression  off
+        timeFormat        general
+        timePrecision     6
+        runTimeModifiable  true
+        maxCo             0.3
+        maxDi             10.0
+        adjustTimeStep    yes
+    """
+    )
 
     # remove these lines to get fluidsimfoam default helpers
     _helper_decompose_par_dict = None
