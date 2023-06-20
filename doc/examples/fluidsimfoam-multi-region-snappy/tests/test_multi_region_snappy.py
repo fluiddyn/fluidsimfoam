313f8774a729aff0c7f9c364cca492b44ea127f0
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-06-20 16:53:06 +0200

    Fix a typo

--- doc/examples/fluidsimfoam-multi-region-snappy/tests/test_multi_region_snappy.py
+++ doc/examples/fluidsimfoam-multi-region-snappy/tests/test_multi_region_snappy.py
@@ -18,7 +18,7 @@ def test_generate_base_case():
 def test_run():
     params = Simul.create_default_params()
     params.output.sub_directory = "tests_fluidsimfoam/multi-region-snappy"
-    # change parameters to get a very short and small simulations
+    # change parameters to get a very short and small simulation
     params.control_dict.end_time = 0.002
     sim = Simul(params)
     # for now we remove decomposeParDict to run in sequential
