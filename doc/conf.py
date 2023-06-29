ef01852e3dbea42e313958e952c151a5982e6271
paugier <pierre.augier@univ-grenoble-alpes.fr>
2023-06-29 12:02:00 +0200

    Doc: turn warnings into errors

--- doc/conf.py
+++ doc/conf.py
@@ -49,6 +49,13 @@ myst_enable_extensions = [
 
 myst_substitutions = {"release": release}
 
+nb_execution_mode = "cache"
+nb_execution_excludepatterns = ["**/*.ipynb"]
+nb_execution_raise_on_error = True
+nb_execution_show_tb = True
+nb_execution_timeout = 300
+nb_merge_streams = True
+
 templates_path = ["_templates"]
 exclude_patterns = [
     "_build",
