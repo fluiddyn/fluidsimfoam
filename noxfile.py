c519893e7146c1be30537afb5acef3eb4f140b8d
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-01-20 23:24:54 +0100

    More nox + release in CI + Github Actions

--- noxfile.py
+++ noxfile.py
@@ -9,8 +9,19 @@ nox.options.reuse_existing_virtualenvs = 1
 
 
 @nox.session
+def test(session):
+    session.run_always(
+        "pdm", "sync", "-G", "test", "-G", "pyvista", "--clean", external=True
+    )
+    command = (
+        "pdm run pytest tests doc/examples --cov --cov-report term --cov-report xml"
+    )
+    session.run(*command.split(), external=True)
+
+
+@nox.session
 def doc(session):
-    session.run_always("pdm", "sync", "-G", "doc", external=True)
+    session.run_always("pdm", "sync", "-G", "doc", "--clean", external=True)
     session.chdir("doc")
     session.run("make", "cleanall", external=True)
     session.run("make", external=True)
